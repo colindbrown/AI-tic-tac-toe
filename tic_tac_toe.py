07b61f5aa2ca41589df69ed8261fa6f090ccc3aa
Colin Brown <colindonovanbrown@gmail.com>
2018-12-27 13:22:27 -0500

    Create basic AI that never loses when going first

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,6 +1,9 @@
 import time
 
 EXAMPLE = True
+MOVELIST = {'a1': 0, 'a2': 1, 'a3': 2, 'b1': 3, 'b2': 4, 'b3': 5, 'c1': 6, 'c2': 7, 'c3': 8}
+TRIADS = [(0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6)]
+OPPOSITE = {"X": "O", "O": "X"}
 
 class Board:
     def __init__(self):
@@ -23,32 +26,61 @@ class Board:
         self.board[pos] = value
         self.played += 1
 
-        # check for winner
-        row = [self.board[pos//3*3], self.board[pos//3*3 + 1], self.board[pos//3*3 + 2]]
-        column = [self.board[pos%3], self.board[pos%3 + 3], self.board[pos%3 + 6]]
-        if row[0] == row[1] and row[1] == row[2]:
-            self.winner = value
-        elif column[0] == column[1] and column[1] == column[2]:
-            self.winner = value
-        elif pos in (0,4,8) and self.board[0] == self.board[4] and self.board[4] == self.board[8]:
-            self.winner = value
-        elif pos in (2,4,6) and self.board[2] == self.board[4] and self.board[4] == self.board[6]:
-            self.winner = value
-
-    def bestMove(self): # to be rewritten
-        for i in range(9):
-            if self.board[i] == " ":
-                return i
-
     def reset(self):
         self.board = [" " for i in range(9)]
-        self.winner = ""
         self.played = 0
 
+class SmartBoard(Board):
+    def playMove(self, pos, value):
+        Board.playMove(self, pos, value)
+        self.proximateCount[pos]["X"] -= 10 # discount squares which have already been used
+
+        for t in TRIADS:
+            if pos in t:
+                same, opp = (0,0)
+                for i in t:
+                    if self.board[i] == value:
+                        same += 1
+                    elif self.board[i] == OPPOSITE[value]:
+                        opp += 1
+                if same == 3:
+                    self.winner = value
+                elif opp == 0:
+                    for i in t:
+                        self.proximateCount[i][value] += 1
+                else:
+                    if t in self.possibleWins:
+                        self.possibleWins.remove(t)
+                    for i in t:
+                        self.proximateCount[i][OPPOSITE[value]] -= 1
+        print(self.proximateCount)
+
+    def bestMove(self):
+        proximateScores = [0 for i in range(9)]
+        for pos in range(9):
+            proximateScores[pos] += self.proximateCount[pos]["X"]
+            if self.unused(pos):
+                if self.proximateCount[pos]["X"] >= 2:
+                    return pos
+                for t in self.possibleWins:
+                    if pos in t:
+                        for i in t:
+                            if i != pos and self.unused(i):
+                                proximateScores[pos] += self.proximateCount[i]["X"] - self.proximateCount[i]["O"]
+
+        print(proximateScores)
+        return proximateScores.index(max(proximateScores))
+
+
+    def reset(self):
+        Board.reset(self)
+        self.winner = ""
+        self.proximateCount = [{"X": 0, "O": 0} for i in range(9)]
+        self.possibleWins = TRIADS.copy()
 
 class Game:
     def __init__(self):
-        self.board = Board()
+        self.board = SmartBoard()
         self.Xturn = True
     
     def intro(self):
@@ -60,17 +92,17 @@ class Game:
         return input().lower() in ('yes', 'y')
 
     def playerTurn(self):
-        moveList = {'a1': 0, 'a2': 1, 'a3': 2, 'b1': 3, 'b2': 4, 'b3': 5, 'c1': 6, 'c2': 7, 'c3': 8}
-
         print("\nYour turn\nWhere would you like to go?")
         choice = input().lower()
-        while not (choice in moveList and self.board.unused(moveList[choice])):
+        while not (choice in MOVELIST and self.board.unused(MOVELIST[choice])):
             print("\nPlease select another position")
             choice = input().lower()
-        self.board.playMove(moveList[choice], 'O')
+        self.board.playMove(MOVELIST[choice], 'O')
         print(self.board.toStr())
 
     def playRound(self):
+        print("\nWould you like to go first? (y/n)")
+        self.Xturn = input().lower() not in ('yes', 'y')
         while True:
             if self.Xturn:
                 print("\nMy turn")
@@ -88,7 +120,7 @@ class Game:
             self.Xturn = not self.Xturn
 
     def play(self):
-        if self.intro():
+        if True:#self.intro():
             while True:
                 self.playRound()
                 print('Would you like to play again?')
