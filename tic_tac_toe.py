e77e16bbf5ad5aefe3819da8e30720097424bc39
Colin Brown <colindonovanbrown@gmail.com>
2018-12-28 00:05:24 -0500

    Clean up unnecessary lines, change variable names for readability

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,20 +1,20 @@
-MOVELIST = {'a1': 0, 'a2': 1, 'a3': 2, 'b1': 3, 'b2': 4, 'b3': 5, 'c1': 6, 'c2': 7, 'c3': 8}
-TRIADS = [(0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6)]
 OPPOSITE = {"X": "O", "O": "X"}
+INDEXDICT = {'a1': 0, 'a2': 1, 'a3': 2, 'b1': 3, 'b2': 4, 'b3': 5, 'c1': 6, 'c2': 7, 'c3': 8}
+TRIADS = [(0,1,2), (3,4,5), (6,7,8), (0,3,6), (1,4,7), (2,5,8), (0,4,8), (2,4,6)]
 
 class Board:
     def __init__(self):
         self.reset()
 
-    def toStr(self):
-        return (f'\n   |   |   \n {self.board[0]} | {self.board[1]} | {self.board[2]} \n___|___|___' +
-                f'\n   |   |   \n {self.board[3]} | {self.board[4]} | {self.board[5]} \n___|___|___' +
-                f'\n   |   |   \n {self.board[6]} | {self.board[7]} | {self.board[8]} \n   |   |   ')
+    def print(self):
+        print(f'\n   |   |   \n {self.board[0]} | {self.board[1]} | {self.board[2]} \n___|___|___' +
+              f'\n   |   |   \n {self.board[3]} | {self.board[4]} | {self.board[5]} \n___|___|___' +
+              f'\n   |   |   \n {self.board[6]} | {self.board[7]} | {self.board[8]} \n   |   |   ')
 
-    def positions(self):
-        return ('\n    |    |    \n A1 | A2 | A3 \n____|____|____' +
-                '\n    |    |    \n B1 | B2 | B3 \n____|____|____' +
-                '\n    |    |    \n C1 | C2 | C3 \n    |    |    ')
+    def printPositions(self):
+        print('\n    |    |    \n A1 | A2 | A3 \n____|____|____' +
+              '\n    |    |    \n B1 | B2 | B3 \n____|____|____' +
+              '\n    |    |    \n C1 | C2 | C3 \n    |    |    ')
 
     def unused(self, pos):
         return self.board[pos] == " "
@@ -84,42 +84,33 @@ class Game:
         self.board = SmartBoard()
         self.Xturn = True
 
-    def playerTurn(self):
-        print("\nYour turn\nWhere would you like to go?")
-        choice = input().lower()
-        while not (choice in MOVELIST and self.board.unused(MOVELIST[choice])):
-            print("\nPlease select another position")
-            choice = input().lower()
-        self.board.playMove(MOVELIST[choice], 'O')
-        print(self.board.toStr())
-
     def playRound(self):
-        print("\nWould you like to go first? (y/n)")
-        self.Xturn = input().lower() not in ('yes', 'y')
+        Xturn = input("\nWould you like to go first (y/n): ").lower() not in ('yes', 'y')
         while True:
-            if self.Xturn:
+            if Xturn:
                 print("\nMy turn")
                 self.board.playMove(self.board.bestMove(), 'X')
-                print(self.board.toStr())
             else:
-                self.playerTurn()
+                choice = input("\nYour turn\nWhere would you like to go: ").lower()
+                while not (choice in INDEXDICT and self.board.unused(INDEXDICT[choice])):
+                    choice = input("\nPlease select another position: ").lower()
+                self.board.playMove(INDEXDICT[choice], 'O')
+            self.board.print()
 
             if self.board.winner:
                 print("\nIt's a tie!" if self.board.winner == "Tie" else f'\n{self.board.winner} wins!')
                 break
-            self.Xturn = not self.Xturn
+            Xturn = not Xturn
 
     def play(self):
         print('\nWelcome to Tic Tac Toe!\nHere are the positions')
-        print(self.board.positions())
+        self.board.printPositions()
         while True:
             self.playRound()
-            print('Would you like to play again?')
-            if input().lower() not in ('yes', 'y'):
+            if input('Would you like to play again (y/n): ').lower() not in ('yes', 'y'):
                 break
             else:
                 self.board.reset()
-
 
 if __name__ == '__main__':
     game = Game()
